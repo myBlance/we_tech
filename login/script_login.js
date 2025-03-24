efd09208ecfe13345bd36934ce5b741075b39b2b
bblanc <tranlongh2003@gmail.com>
2025-03-24 22:22:59 +0700

    cap nhat code

--- login/script_login.js
+++ login/script_login.js
@@ -1,3 +1,21 @@
-function redirectTo(page) {
-    window.location.href = page;
+function redirectTo(url) {
+    window.location.href = url;
+}
+
+
+function showForgotPasswordForm() {
+    document.querySelector(".login-box").style.display = "none"; // Ẩn form đăng nhập
+    document.getElementById("forgot-password-box").style.display = "block"; // Hiện form quên mật khẩu
+}
+
+function showLoginForm() {
+    document.getElementById("forgot-password-box").style.display = "none"; // Ẩn form quên mật khẩu
+    document.querySelector(".login-box").style.display = "block"; // Hiện lại form đăng nhập
+}
+
+
+function showResetPasswordForm(event) {
+    event.preventDefault(); // Ngăn chặn form gửi đi
+    document.getElementById("forgot-password-box").style.display = "none"; // Ẩn form quên mật khẩu
+    document.getElementById("reset-password-box").style.display = "block"; // Hiện form đổi mật khẩu
 }
